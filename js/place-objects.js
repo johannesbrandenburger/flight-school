ecb732ef7013bdb6a0c92d9da2a1f03373b795a1
Johannes Brandenburger <johannes0709@icloud.com>
2022-11-07 20:52:54 +0100

    tables and some other stuff

--- js/place-objects.js
+++ js/place-objects.js
@@ -27,13 +27,208 @@ async function placeObjects() {
     // room walls
     let room = await getMashFromBlenderModel("../blender/room_door.glb");
     scene.add(room); myObjects.room = room;
-    myObjects.room.position.set(0, 0, 0);
+    myObjects.room.position.set(5.213, 0, 5.750);
     getAllMeshsFromNestedGroup(myObjects.room).forEach(mesh => {
         mesh.castShadow = true;
         if (mesh.name === "") {
             mesh.name = "part of room";
         }
     });
+
+    // blackboard
+    let blackboard = await getMashFromBlenderModel("../blender/blackboard.glb");
+    scene.add(blackboard); myObjects.blackboard = blackboard;
+    myObjects.blackboard.position.set(3.400, 0, 0.4);
+    getAllMeshsFromNestedGroup(myObjects.blackboard).forEach(mesh => {
+        mesh.receiveShadow = true;
+    });
+    
+    // closet 1
+    let closetOne = await getMashFromBlenderModel("../blender/Closet.glb");
+    scene.add(closetOne); myObjects.closetOne = closetOne;
+    myObjects.closetOne.rotateY(degToRad(180));
+    myObjects.closetOne.position.set(8.926, 0, 0);
+
+    // closet 2
+    let closetTwo = await getMashFromBlenderModel("../blender/Closet.glb");
+    scene.add(closetTwo); myObjects.closetTwo = closetTwo;
+    myObjects.closetTwo.rotateY(degToRad(180));
+    myObjects.closetTwo.position.set(9.926, 0, 0);
+    myObjects.closetTwo.castShadow = true;
+
+    // sideboard 1
+    let sideboardOne = await getMashFromBlenderModel("../blender/sideboard.glb");
+    scene.add(sideboardOne); myObjects.sideboardOne = sideboardOne;
+    myObjects.sideboardOne.rotateY(degToRad(270));
+    myObjects.sideboardOne.position.set(0, 0, 0);
+    myObjects.sideboardOne.receiveShadow = true;
+
+    // sideboard 2
+    let sideboardTwo = await getMashFromBlenderModel("../blender/sideboard.glb");
+    scene.add(sideboardTwo); myObjects.sideboardTwo = sideboardTwo;
+    myObjects.sideboardTwo.rotateY(degToRad(270));
+    myObjects.sideboardTwo.position.set(1.5, 0, 0);
+    myObjects.sideboardTwo.receiveShadow = true;
+
+    // sideboard 3
+    let sideboardThree = await getMashFromBlenderModel("../blender/sideboard.glb");
+    scene.add(sideboardThree); myObjects.sideboardThree = sideboardThree;
+    myObjects.sideboardThree.rotateY(degToRad(0));
+    myObjects.sideboardThree.position.set(0, 0, 7.45);
+    myObjects.sideboardThree.receiveShadow = true;
+
+    // prof chair
+    let profChair = await getMashFromBlenderModel("../blender/chair.glb");
+    scene.add(profChair); myObjects.profChair = profChair;
+    myObjects.profChair.rotateY(degToRad(180));
+    myObjects.profChair.scale.set(1.4, 1.4, 1.4);
+    myObjects.profChair.position.set(3.4, -1.3, 1.7);
+
+    // prof table
+    let profTable = await getMashFromBlenderModel("../blender/table.glb");
+    scene.add(profTable); myObjects.profTable = profTable;
+    myObjects.profTable.position.set(3, 0, 2.5);
+
+    // keyboard
+    let keyboard = await getMashFromBlenderModel("../blender/keyboard.glb");
+    scene.add(keyboard); myObjects.keyboard = keyboard;
+    myObjects.keyboard.position.set(3.4, 0.8, 2.8);
+    myObjects.keyboard.scale.set(0.05, 0.05, 0.05)
+    myObjects.keyboard.rotateY(degToRad(180));
+    getAllMeshsFromNestedGroup(myObjects.keyboard).forEach(mesh => {
+        mesh.receiveShadow = true;
+    });
+
+    myObjects.tables = [];
+
+    // table 1
+    let tableOne = await getMashFromBlenderModel("../blender/table.glb");
+    tableOne.rotateY(degToRad(90));
+    tableOne.position.set(2.026, 0, 5.9);
+    scene.add(tableOne);
+    myObjects.tables.push(tableOne);
+
+    // table 2
+    let tableTwo = await getMashFromBlenderModel("../blender/table.glb");
+    tableTwo.rotateY(degToRad(90));
+    tableTwo.position.set(2.026, 0, 7.7);
+    scene.add(tableTwo);
+    myObjects.tables.push(tableTwo);
+
+    // table 3
+    let tableThree = await getMashFromBlenderModel("../blender/table.glb");
+    tableThree.rotateY(degToRad(90));
+    tableThree.position.set(2.026, 0, 9.5);
+    scene.add(tableThree);
+    myObjects.tables.push(tableThree);
+
+    // table 4
+    let tableFour = await getMashFromBlenderModel("../blender/table.glb");
+    tableFour.position.set(2.926, 0, 4.1);
+    scene.add(tableFour);
+    myObjects.tables.push(tableFour);
+    
+    // table 5
+    let tableFive = await getMashFromBlenderModel("../blender/table.glb");
+    tableFive.position.set(2.926, 0, 6.5);
+    scene.add(tableFive);
+    myObjects.tables.push(tableFive);
+    
+    // table 6
+    let tableSix = await getMashFromBlenderModel("../blender/table.glb");
+    tableSix.position.set(2.926, 0, 8.6);
+    scene.add(tableSix);
+    myObjects.tables.push(tableSix);
+    
+    // table 7
+    let tableSeven = await getMashFromBlenderModel("../blender/table.glb");
+    tableSeven.rotateY(degToRad(90));
+    tableSeven.position.set(7.726, 0, 5.9);
+    scene.add(tableSeven);
+    myObjects.tables.push(tableSeven);
+    
+    // table 8
+    let tableEight = await getMashFromBlenderModel("../blender/table.glb");
+    tableEight.rotateY(degToRad(90));
+    tableEight.position.set(7.726, 0, 7.7);
+    scene.add(tableEight);
+    myObjects.tables.push(tableEight);
+
+    // table 9
+    let tableNine = await getMashFromBlenderModel("../blender/table.glb");
+    tableNine.rotateY(degToRad(90));
+    tableNine.position.set(7.726, 0, 9.5);
+    scene.add(tableNine);
+    myObjects.tables.push(tableNine);
+
+    // table 10
+    let tableTen = await getMashFromBlenderModel("../blender/table.glb");
+    tableTen.position.set(5.926, 0, 4.1);
+    scene.add(tableTen);
+    myObjects.tables.push(tableTen);
+    
+    // table 11
+    let tableEleven = await getMashFromBlenderModel("../blender/table.glb");
+    tableEleven.position.set(5.926, 0, 6.5);
+    scene.add(tableEleven);
+    myObjects.tables.push(tableEleven);
+    
+    // table 12
+    let tableTwelve = await getMashFromBlenderModel("../blender/table.glb");
+    tableTwelve.position.set(5.926, 0, 8.6);
+    scene.add(tableTwelve);
+    myObjects.tables.push(tableTwelve);
+    
+    // add shadow to tables
+    myObjects.tables.forEach(table => {
+        getAllMeshsFromNestedGroup(table).forEach(mesh => {
+            mesh.castShadow = true;
+            mesh.receiveShadow = true;
+        });
+    });
+
+
+
+
+    // add window glass
+    // let squareGeometry = new THREE.PlaneGeometry(1, 0.5);
+    // const glassMaterial = new THREE.MeshPhysicalMaterial( {
+    //     color: 0xffffff, metalness: 0.25, roughness: 0, transmission: 1.0
+    // } );
+    // let square = new THREE.Mesh(squareGeometry, glassMaterial);
+    // square.receiveShadow = true;
+    // scene.add(square); myObjects.square = square;
+    // myObjects.square.position.set(0, 1.5, 4);
+    // myObjects.square.rotateY(degToRad(90));
+
+
+
+    // const mirrorBack1 = new THREE.Reflector(
+    //     new THREE.PlaneBufferGeometry(1, 0.5),
+    //     {
+    //         color: new THREE.Color(0x7f7f7f),
+    //         textureWidth: window.innerWidth * window.devicePixelRatio,
+    //         textureHeight: window.innerHeight * window.devicePixelRatio
+    //     }
+    // )
+    
+    // scene.add(mirrorBack1); myObjects.mirrorBack1 = mirrorBack1;
+    // myObjects.mirrorBack1.position.set(0, 1.5, 4);
+    // myObjects.mirrorBack1.rotateY(degToRad(90));
+
+    
+    // // set opacity of mirror
+    // myObjects.mirrorBack1.material.opacity = 0.5;
+    // myObjects.mirrorBack1.material.transparent = true;
+
+
+
+
+
+
+
+
+
 
     // landscape environment
     let environment = await getMashFromBlenderModel("../blender/mountain_landscape.glb");
