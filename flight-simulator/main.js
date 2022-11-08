839701f2bc8f91ba0ab1c02551f31d6f425e6ac2
Johannes Brandenburger <johannes0709@icloud.com>
2022-11-09 00:38:45 +0100

    ocean movement

--- flight-simulator/main.js
+++ flight-simulator/main.js
@@ -25,13 +25,15 @@ const torusScale = 0.2;
 const torusRadius = 2 * torusScale;
 const torusTube = 0.3 * torusScale;
 const torusSpawnRadius = 100 * torusScale;
-const torusAmount = 500;
+const torusAmount = 300;
+const extraTorusAmount = 30;
 let torusScore = 0;
 let hasScored = false;
 let startTime = null;
 let timeLeft = 60;
 let sun, water;
 let planeWingSize = 0.1;
+let isFlying = true;
 
 
 init().then(() => {
@@ -131,6 +133,18 @@ async function init() {
         }
     });
 
+    // add event listener to pause the game
+    window.addEventListener("keydown", (e) => {
+        if (e.key === "p") {
+            if (isFlying) {
+                isFlying = false;
+                document.getElementById("time").innerHTML = "Paused";
+            } else {
+                isFlying = true;
+            }
+        }
+    });
+
     // add the canvas and remove the loading div
     document.body.appendChild(renderer.domElement);
     document.body.removeChild(loadingDiv);
@@ -142,7 +156,7 @@ async function init() {
  * Places torus objects in the scene at random positions+
  */
 function placeTorusObjects() {
-    for (let i = 0; i < torusAmount; i++) {
+    for (let i = 0; i < torusAmount + extraTorusAmount; i++) {
         const torus = new THREE.Mesh(
             new THREE.TorusGeometry(torusRadius, torusTube, 16, 100),
             new THREE.MeshPhongMaterial({ color: 0xff0000 })
@@ -160,6 +174,12 @@ function placeTorusObjects() {
         torus.rotation.y = Math.random() > 0.5 ? Math.PI / 2 : 0;
         scene.add(torus);
         torus.name = "torus";
+
+        // if torus is in the extra torus amount, make it gold and name it "extraTorus"
+        if (i >= torusAmount) {
+            torus.material.color.setHex(0xffd700);
+            torus.name = "extraTorus";
+        }
 
         // check if torus intersects with another torus
         let torusIntersects = false;
@@ -194,13 +214,13 @@ function handleScore() {
     if (planePosition.y < 0) {
         gameOver();
         return;
-    } 
+    }
 
     let nearestTorus = null;
 
     // check if plane intersects with a torus
     for (let i = 0; i < scene.children.length; i++) {
-        if (scene.children[i].name !== "torus") continue;
+        if (scene.children[i].name !== "torus" && scene.children[i].name !== "extraTorus") continue;
 
         // get nearest torus
         const torus = scene.children[i];
@@ -226,7 +246,7 @@ function handleScore() {
         if (distanceToCenter < torusRadius - 0.5 * torusTube && !hasScored) {
             nearestTorus.material.color.set(0x00ff00);
             nearestTorus.material.needsUpdate = true;
-            torusScore += 1;
+            torusScore = nearestTorus.name === "extraTorus" ? torusScore + 5 : torusScore + 1;
             hasScored = true;
             document.getElementById("score").innerHTML = "Score: " + torusScore;
 
@@ -235,8 +255,8 @@ function handleScore() {
                 scene.remove(nearestTorus);
                 hasScored = false;
             }, 500);
-        } 
-        
+        }
+
         if (distanceToCenter > torusRadius - planeWingSize - 0.5 * torusTube && distanceToCenter < torusRadius + torusTube + planeWingSize) {
             gameOver();
         }
@@ -248,10 +268,15 @@ function handleScore() {
  * Animates the scene
  */
 async function animate() {
-    requestAnimationFrame(animate);
-    handleFlying();
-    handleScore();
-    handleTime();
+
+    requestAnimationFrame( animate );
+
+    if (isFlying) {
+        handleFlying();
+        handleScore();
+        handleTime();
+    }
+    water.material.uniforms[ 'time' ].value += 0.005
     renderer.render(scene, camera);
     await new Promise(resolve => setTimeout(resolve, animationTimeoutMs));
 }
@@ -276,8 +301,57 @@ function handleTime() {
  * Quits the game and shows a game over message TODO: add a ui for this
  */
 function gameOver() {
-    console.log("Game over! Your score is: " + torusScore);
-    window.location.href = "/";
+
+    // remove all event listeners
+    window.removeEventListener("mousedown", () => {
+        distancePerFly = distancePerFly * 2;
+    });
+    window.removeEventListener("mouseup", () => {
+        distancePerFly = distancePerFly / 2;
+    });
+
+    // remove the plane
+    scene.remove(myObjects.modelPlane);
+
+    // stop the game
+    isFlying = false;
+
+    // set cursor to default
+    document.body.style.cursor = "pointer";
+
+    // show game over message and score in the middle of the screen
+    const gameOverDiv = document.createElement("div");
+    gameOverDiv.id = "gameOverDiv";
+    gameOverDiv.innerHTML = "Game over! Your score is: " + torusScore;
+    gameOverDiv.style.position = "absolute";
+    gameOverDiv.style.top = "50%";
+    gameOverDiv.style.left = "50%";
+    gameOverDiv.style.transform = "translate(-50%, -50%)";
+    gameOverDiv.style.fontSize = "50px";
+    gameOverDiv.style.color = "white";
+    document.body.appendChild(gameOverDiv);
+
+    // show restart button
+    const restartButton = document.createElement("button");
+    restartButton.id = "restartButton";
+    restartButton.innerHTML = "Restart";
+    restartButton.style.position = "absolute";
+    restartButton.style.top = "50%";
+    restartButton.style.left = "50%";
+    restartButton.style.transform = "translate(-50%, -50%)";
+    restartButton.style.fontSize = "50px";
+    restartButton.style.color = "white";
+    restartButton.style.backgroundColor = "black";
+    restartButton.style.border = "none";
+    restartButton.style.padding = "20px";
+    restartButton.style.cursor = "pointer";
+    restartButton.style.marginTop = "100px";
+    restartButton.onclick = () => {
+        location.reload();
+    }
+    document.body.appendChild(restartButton);
+
+
 }
 
 
