fc11f0327619a2260841af7c5cd49710112464de
Johannes Brandenburger <johannes0709@icloud.com>
2022-11-11 12:24:13 +0100

    dynamic chair and table creation

--- js/place-objects.js
+++ js/place-objects.js
@@ -9,13 +9,6 @@ async function placeObjects() {
     let room = await getMashFromBlenderModel("../blender/room_door.glb");
     scene.add(room); myObjects.room = room;
     myObjects.room.position.set(5.213, 0, 5.750);
-    // getAllMeshsFromNestedGroup(myObjects.room).forEach(mesh => {
-    //     console.log(mesh.name);
-    //     mesh.receiveShadow = true;
-    //     if (mesh.name === "") {
-    //         mesh.name = "part of room";
-    //     }
-    // });
 
     // adjust the shadow based on the mash
     myObjects.room.children.find(child => child.name === "Floor").receiveShadow = true;
@@ -132,89 +125,33 @@ async function placeObjects() {
     myObjects.monitor.rotateY(degToRad(270));
     myObjects.monitor.traverse(child => { child.castShadow = true; });
 
-    //#region tables
+    // #region tables
 
     myObjects.tables = [];
 
-    // table 1
-    let tableOne = tableModel.clone();
-    tableOne.rotateY(degToRad(90));
-    tableOne.position.set(2.026, 0, 5.9);
-    scene.add(tableOne);
-    myObjects.tables.push(tableOne);
+    const tablePositions = [
+        {x: 2.026, y: 0, z: 5.9, rotation: 90},
+        {x: 2.026, y: 0, z: 7.7, rotation: 90},
+        {x: 2.026, y: 0, z: 9.5, rotation: 90},
+        {x: 2.926, y: 0, z: 4.1, rotation: 0},
+        {x: 2.926, y: 0, z: 6.5, rotation: 0},
+        {x: 2.926, y: 0, z: 8.6, rotation: 0},
+        {x: 7.726, y: 0, z: 5.9, rotation: 90},
+        {x: 7.726, y: 0, z: 7.7, rotation: 90},
+        {x: 7.726, y: 0, z: 9.5, rotation: 90},
+        {x: 5.926, y: 0, z: 4.1, rotation: 0},
+        {x: 5.926, y: 0, z: 6.5, rotation: 0},
+        {x: 5.926, y: 0, z: 8.6, rotation: 0},  
+    ];
 
-    // table 2
-    let tableTwo = tableModel.clone();
-    tableTwo.rotateY(degToRad(90));
-    tableTwo.position.set(2.026, 0, 7.7);
-    scene.add(tableTwo);
-    myObjects.tables.push(tableTwo);
-
-    // table 3
-    let tableThree = tableModel.clone();
-    tableThree.rotateY(degToRad(90));
-    tableThree.position.set(2.026, 0, 9.5);
-    scene.add(tableThree);
-    myObjects.tables.push(tableThree);
-
-    // table 4
-    let tableFour = tableModel.clone();
-    tableFour.position.set(2.926, 0, 4.1);
-    scene.add(tableFour);
-    myObjects.tables.push(tableFour);
-
-    // table 5
-    let tableFive = tableModel.clone();
-    tableFive.position.set(2.926, 0, 6.5);
-    scene.add(tableFive);
-    myObjects.tables.push(tableFive);
-
-    // table 6
-    let tableSix = tableModel.clone();
-    tableSix.position.set(2.926, 0, 8.6);
-    scene.add(tableSix);
-    myObjects.tables.push(tableSix);
-
-    // table 7
-    let tableSeven = tableModel.clone();
-    tableSeven.rotateY(degToRad(90));
-    tableSeven.position.set(7.726, 0, 5.9);
-    scene.add(tableSeven);
-    myObjects.tables.push(tableSeven);
-
-    // table 8
-    let tableEight = tableModel.clone();
-    tableEight.rotateY(degToRad(90));
-    tableEight.position.set(7.726, 0, 7.7);
-    scene.add(tableEight);
-    myObjects.tables.push(tableEight);
-
-    // table 9
-    let tableNine = tableModel.clone();
-    tableNine.rotateY(degToRad(90));
-    tableNine.position.set(7.726, 0, 9.5);
-    scene.add(tableNine);
-    myObjects.tables.push(tableNine);
-
-    // table 10
-    let tableTen = tableModel.clone();
-    tableTen.position.set(5.926, 0, 4.1);
-    scene.add(tableTen);
-    myObjects.tables.push(tableTen);
-
-    // table 11
-    let tableEleven = tableModel.clone();
-    tableEleven.position.set(5.926, 0, 6.5);
-    scene.add(tableEleven);
-    myObjects.tables.push(tableEleven);
-
-    // table 12
-    let tableTwelve = tableModel.clone();
-    tableTwelve.position.set(5.926, 0, 8.6);
-    scene.add(tableTwelve);
-    myObjects.tables.push(tableTwelve);
-
-    console.log("myObjects.tableTwelve);", tableTwelve);
+    // create tables 
+    tablePositions.forEach(position => {
+        let table = tableModel.clone();
+        table.rotateY(degToRad(position.rotation));
+        table.position.set(position.x, position.y, position.z);
+        scene.add(table);
+        myObjects.tables.push(table);
+    });
 
     // add shadow to tables
     [...myObjects.tables, myObjects.profTable].forEach(table => {
@@ -227,186 +164,54 @@ async function placeObjects() {
         });
     });
 
-    //#endregion
+    // #endregion
 
-    //#region chairs
+    // #region chairs
 
     myObjects.chairs = [];
 
-    // chair 1
-    let chairOne = chairModel.clone();
-    chairOne.rotateY(degToRad(270));
-    chairOne.position.set(1.8, 0, 4.65);
-    scene.add(chairOne);
-    myObjects.chairs.push(chairOne);
+    const chairPositions = [
+        {x: 1.8, y: 0, z: 4.65, rotation: 270},
+        {x: 1.8, y: 0, z: 5.35, rotation: 270},
+        {x: 1.8, y: 0, z: 6.45, rotation: 270},
+        {x: 1.8, y: 0, z: 7.15, rotation: 270},
+        {x: 1.8, y: 0, z: 8.25, rotation: 270},
+        {x: 1.8, y: 0, z: 8.95, rotation: 270},
+        {x: 3.5, y: 0, z: 5.3, rotation: 0},
+        {x: 4.2, y: 0, z: 5.3, rotation: 0},
+        {x: 3.5, y: 0, z: 7.65, rotation: 0},
+        {x: 4.2, y: 0, z: 7.65, rotation: 0},
+        {x: 3.5, y: 0, z: 9.75, rotation: 0},
+        {x: 4.2, y: 0, z: 9.75, rotation: 0},
+        {x: 8.9, y: 0, z: 4.65, rotation: 90},
+        {x: 8.9, y: 0, z: 5.35, rotation: 90},
+        {x: 8.9, y: 0, z: 6.45, rotation: 90},
+        {x: 8.9, y: 0, z: 7.15, rotation: 90},
+        {x: 8.9, y: 0, z: 8.25, rotation: 90},
+        {x: 8.9, y: 0, z: 8.95, rotation: 90},
+        {x: 6.5, y: 0, z: 5.3, rotation: 0},
+        {x: 7.2, y: 0, z: 5.3, rotation: 0},
+        {x: 6.5, y: 0, z: 7.65, rotation: 0},
+        {x: 7.2, y: 0, z: 7.65, rotation: 0},
+        {x: 6.5, y: 0, z: 9.75, rotation: 0},
+        {x: 7.2, y: 0, z: 9.75, rotation: 0},
+    ];
 
-    // chair 2
-    let chairTwo = chairModel.clone();
-    chairTwo.rotateY(degToRad(270));
-    chairTwo.position.set(1.8, 0, 5.35);
-    scene.add(chairTwo);
-    myObjects.chairs.push(chairTwo);
+    // create chairs
+    chairPositions.forEach(position => {
+        let chair = chairModel.clone();
+        chair.rotateY(degToRad(position.rotation));
+        chair.position.set(position.x, position.y, position.z);
+        scene.add(chair);
+        myObjects.chairs.push(chair);
+    });
 
-    // chair 3
-    let chairThree = chairModel.clone();
-    chairThree.rotateY(degToRad(270));
-    chairThree.position.set(1.8, 0, 6.45);
-    scene.add(chairThree);
-    myObjects.chairs.push(chairThree);
-
-    // chair 4
-    let chairFour = chairModel.clone();
-    chairFour.rotateY(degToRad(270));
-    chairFour.position.set(1.8, 0, 7.15);
-    scene.add(chairFour);
-    myObjects.chairs.push(chairFour);
-
-    // chair 5
-    let chairFive = chairModel.clone();
-    chairFive.rotateY(degToRad(270));
-    chairFive.position.set(1.8, 0, 8.25);
-    scene.add(chairFive);
-    myObjects.chairs.push(chairFive);
-
-    // chair 6
-    let chairSix = chairModel.clone();
-    chairSix.rotateY(degToRad(270));
-    chairSix.position.set(1.8, 0, 8.95);
-    scene.add(chairSix);
-    myObjects.chairs.push(chairSix);
-
-    // chair 7
-    let chairSeven = chairModel.clone();
-    chairSeven.rotateY(degToRad(0));
-    chairSeven.position.set(3.5, 0, 5.3);
-    scene.add(chairSeven);
-    myObjects.chairs.push(chairSeven);
-
-    // chair 8
-    let chairEight = chairModel.clone();
-    chairEight.rotateY(degToRad(0));
-    chairEight.position.set(4.2, 0, 5.3);
-    scene.add(chairEight);
-    myObjects.chairs.push(chairEight);
-
-    // chair 9
-    let chairNine = chairModel.clone();
-    chairNine.rotateY(degToRad(0));
-    chairNine.position.set(3.5, 0, 7.65);
-    scene.add(chairNine);
-    myObjects.chairs.push(chairNine);
-
-    // chair 10
-    let chairTen = chairModel.clone();
-    chairTen.rotateY(degToRad(0));
-    chairTen.position.set(4.2, 0, 7.65);
-    scene.add(chairTen);
-    myObjects.chairs.push(chairTen);
-
-    // chair 11
-    let chairEleven = chairModel.clone();
-    chairEleven.rotateY(degToRad(0));
-    chairEleven.position.set(3.5, 0, 9.75);
-    scene.add(chairEleven);
-    myObjects.chairs.push(chairEleven);
-
-    // chair 12
-    let chairTwelve = chairModel.clone();
-    chairTwelve.rotateY(degToRad(0));
-    chairTwelve.position.set(4.2, 0, 9.75);
-    scene.add(chairTwelve);
-    myObjects.chairs.push(chairTwelve);
-
-    // chair 13
-    let chairThirteen = chairModel.clone();
-    chairThirteen.rotateY(degToRad(90));
-    chairThirteen.position.set(8.9, 0, 4.65);
-    scene.add(chairThirteen);
-    myObjects.chairs.push(chairThirteen);
-
-    // chair 14
-    let chairFourteen = chairModel.clone();
-    chairFourteen.rotateY(degToRad(90));
-    chairFourteen.position.set(8.9, 0, 5.35);
-    scene.add(chairFourteen);
-    myObjects.chairs.push(chairFourteen);
-
-    // chair 15
-    let chairFifteen = chairModel.clone();
-    chairFifteen.rotateY(degToRad(90));
-    chairFifteen.position.set(8.9, 0, 6.45);
-    scene.add(chairFifteen);
-    myObjects.chairs.push(chairFifteen);
-
-    // chair 16
-    let chairSixteen = chairModel.clone();
-    chairSixteen.rotateY(degToRad(90));
-    chairSixteen.position.set(8.9, 0, 7.15);
-    scene.add(chairSixteen);
-    myObjects.chairs.push(chairSixteen);
-
-    // chair 17
-    let chairSeventeen = chairModel.clone();
-    chairSeventeen.rotateY(degToRad(90));
-    chairSeventeen.position.set(8.9, 0, 8.25);
-    scene.add(chairSeventeen);
-    myObjects.chairs.push(chairSeventeen);
-
-    // chair 18
-    let chairEighteen = chairModel.clone();
-    chairEighteen.rotateY(degToRad(90));
-    chairEighteen.position.set(8.9, 0, 8.95);
-    scene.add(chairEighteen);
-    myObjects.chairs.push(chairEighteen);
-
-    // chair 19
-    let chairNineteen = chairModel.clone();
-    chairNineteen.rotateY(degToRad(0));
-    chairNineteen.position.set(6.5, 0, 5.3);
-    scene.add(chairNineteen);
-    myObjects.chairs.push(chairNineteen);
-
-    // chair 20
-    let chairTwenty = chairModel.clone();
-    chairTwenty.rotateY(degToRad(0));
-    chairTwenty.position.set(7.2, 0, 5.3);
-    scene.add(chairTwenty);
-    myObjects.chairs.push(chairTwenty);
-
-    // chair 21
-    let chairTwentyOne = chairModel.clone();
-    chairTwentyOne.rotateY(degToRad(0));
-    chairTwentyOne.position.set(6.5, 0, 7.65);
-    scene.add(chairTwentyOne);
-    myObjects.chairs.push(chairTwentyOne);
-
-    // chair 22
-    let chairTwentyTwo = chairModel.clone();
-    chairTwentyTwo.rotateY(degToRad(0));
-    chairTwentyTwo.position.set(7.2, 0, 7.65);
-    scene.add(chairTwentyTwo);
-    myObjects.chairs.push(chairTwentyTwo);
-
-    // chair 23
-    let chairTwentyThree = chairModel.clone();
-    chairTwentyThree.rotateY(degToRad(0));
-    chairTwentyThree.position.set(6.5, 0, 9.75);
-    scene.add(chairTwentyThree);
-    myObjects.chairs.push(chairTwentyThree);
-
-    // chair 24
-    let chairTwentyFour = chairModel.clone();
-    chairTwentyFour.rotateY(degToRad(0));
-    chairTwentyFour.position.set(7.2, 0, 9.75);
-    scene.add(chairTwentyFour);
-    myObjects.chairs.push(chairTwentyFour);
-
-    // to low performance to add shadows to all chairs
+    // add shadow to chairs (to low performance to add shadows to all chairs)
     // [...myObjects.chairs, myObjects.profChair].forEach((chair) => {
     //     chair.traverse((child) => { child.castShadow = true; });
     // });
 
-    //#endregion
+    // #endregion
 
     // lightswitch 1
     let lightSwitchOne = await getMashFromBlenderModel("../blender/lightswitch.glb")
@@ -425,7 +230,7 @@ async function placeObjects() {
     lightSwitchThree.position.set(9.6, 1.2, 11.5);
 
 
-    //#region 360 degree environment/background
+    // #region 360 degree environment/background
 
     // sphere shape
     let geometry = new THREE.SphereGeometry(400, 800, 800);
@@ -439,7 +244,7 @@ async function placeObjects() {
     backgroundSphere.position.set(5.213, 10, 5.75);
     scene.add(backgroundSphere);
 
-    //#endregion
+    // #endregion
 
     // create blue sky box
     const skyBox = new THREE.Mesh(
